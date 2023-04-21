a15d88035eb3b5c34c109acb08bc88b196339b4c
k31i <115046776+k31i@users.noreply.github.com>
2023-04-21 16:03:56 +0000

    weights

--- python code/pythoncode.py
+++ python code/pythoncode.py
@@ -10,15 +10,18 @@ def sigmoid(x):
   # Sigmoid activation function: f(x) = 1 / (1 + e^(-x))
   return 1 / (1 + np.exp(-x))
 
-number_of_weights = n_hidden + 1
+number_of_weights = n_hidden -1
 #how this should be done is we need to create the first layer, repeat for number of layers and last layer
 
-weights_1 = np.random.uniform(low=-2,high=2,size=(n_inputs,n_hidden_nodes))
-print(weights_1)
+#weights list
+weight_lis = []
+weight_lis.append(np.random.uniform(low=-2,high=2,size=(n_inputs,n_hidden_nodes)))
 
-#for n in int(n_hidden-1):
- # new_row = np.random.uniform(low=-2,high=2,size=(n_hidden_nodes,n_hidden_nodes))
-  #weights_1 = np.vstack((weights_1,new_row))
+for n in range(number_of_weights):
+  new_row = np.random.uniform(low=-2,high=2,size=(n_hidden_nodes,n_hidden_nodes))
+  weight_lis.append(new_row)
 
-#new_row = np.random.uniform(low=-2,high=2,size=(n_hidden_nodes,n_outputs))
-#weights_1 = weights_1 = np.vstack((weights_1,new_row))
+new_row = np.random.uniform(low=-2,high=2,size=(n_hidden_nodes,n_outputs))
+weight_lis.append(new_row)
+print(weight_lis)
+#appending will allow us to create a list of matricies
