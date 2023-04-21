090c182d70af4974f9120db7a16f6a0a5a6d19c8
k31i <115046776+k31i@users.noreply.github.com>
2023-04-21 15:24:53 +0000

    creating the neural netwrok

--- python code/pythoncode.py
+++ python code/pythoncode.py
@@ -1,2 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+n_inputs = 9
+n_outputs = 9
+n_hidden_nodes = 9
+n_hidden = 2
+
+def sigmoid(x):
+  # Sigmoid activation function: f(x) = 1 / (1 + e^(-x))
+  return 1 / (1 + np.exp(-x))
+
+number_of_weights = n_hidden + 1
+#how this should be done is we need to create the first layer, repeat for number of layers and last layer
+
