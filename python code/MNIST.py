de844564b0b6fb776dfd2b504ba7140cf8ca9d0a
k31i <115046776+k31i@users.noreply.github.com>
2023-04-22 22:53:23 +0000

    update

--- python code/MNIST.py
+++ python code/MNIST.py
@@ -5,8 +5,11 @@ import pandas as pd
 def sigmoid(x):
   # Sigmoid activation function: f(x) = 1 / (1 + e^(-x))
   return 1 / (1 + np.exp(-x))
+#another function needs to be added for the output value as we want the highest probability chosen
 
-
+data = pd.read_csv("/workspaces/neural-networks-/python code/data/train.csv")
+data = np.array(data)
+m,n = data.shape
 #note the matricies will need to be eduted so we can include the first list of 700 e.c.t
 
 weight_lis = []
