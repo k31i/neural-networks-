2419b1de87f34a74f82e0d121a9177337ef889f3
k31i <115046776+k31i@users.noreply.github.com>
2023-04-22 22:39:18 +0000

    update

--- python code/MNIST.py
+++ python code/MNIST.py
@@ -2,3 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+def sigmoid(x):
+  # Sigmoid activation function: f(x) = 1 / (1 + e^(-x))
+  return 1 / (1 + np.exp(-x))
+
+
+#note the matricies will need to be eduted so we can include the first list of 700 e.c.t
+
+weight_lis = []
+for i in range(3):
+    matrix = np.random.uniform(low=-2, high=2, size=(10, 10))
+    weight_lis.append(matrix)
+
+bais_lis = []
+for i in range(3):
+    matrix = np.random.uniform(low=-2, high=2, size=(10, 10))
+    bais_lis.append(matrix)
+
+node_lis_1 = []
+node_lis_2 = np.empty(10)
+node_lis_3 = np.empty(10)
+node_lis_4 = np.empty(10)
