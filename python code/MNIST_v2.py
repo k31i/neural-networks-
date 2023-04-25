c1edc1c6796c164f67e58bbe0ecc4f2d89f6bc63
k31i <115046776+k31i@users.noreply.github.com>
2023-04-25 00:22:50 +0000

    its 1:22am

--- python code/MNIST_v2.py
+++ python code/MNIST_v2.py
@@ -27,7 +27,7 @@ _,m_train = X_train.shape
 #-------define the pramaters aka weights and nodes
 
 inp_a = []
-nodes_1_a = np.empty(16)
+nodes_1_a = np.empty(16)#nodes not needed now
 nodes_1_z = np.empty(16)
 nodes_2_a = np.empty(16)
 nodes_2_b = np.empty(16)
@@ -58,20 +58,22 @@ def one_hot(Y):#################################coppied from kaggel and used in 
     one_hot_Y = one_hot_Y.T
     return one_hot_Y
 
-def forward_prop(a_0,a_1,a_2,a_3,z_1,z_2,z_3,w_1,w_2,w_3,b_1,b_2,b_3):#correct for dot prod
+def forward_prop(a_0,w_1,w_2,w_3,b_1,b_2,b_3):#correct for dot prod
    z_1 = w_1.dot(a_0) - b_1
-   z_2 = w_2.dot(a_1) - b_2
-   z_3 = w_3.dot(a_2) - b_3 
-   
    a_1 = sigmoid(z_1)
+   
+   z_2 = w_2.dot(a_1) - b_2
    a_2 = sigmoid(z_2)
+
+   z_3 = w_3.dot(a_2) - b_3 
    a_3 = softactive(z_3)
    return z_1,z_2,z_3,a_1,a_2,a_3
 
 def sigmoid_dev(x):
    return (1/(1+np.e**(-x)))*(1-(1/(1+np.e**(-x))))
 
-#we need one for soft active derivative we will call it softactive_dev!!!!!!!!
+def softactive_dev(x):#FUCK THIS!!!
+   return 1
 
 def back_prop(Y,a_3,a_2,a_1,a_0,z_3,z_2,z_1,w_3,w_2):
    si_3 = np.dot(2*(a_3-Y),softactive_dev(z_3))
@@ -85,7 +87,28 @@ def back_prop(Y,a_3,a_2,a_1,a_0,z_3,z_2,z_1,w_3,w_2):
    w_1_dev = np.dot(a_0,si_1)
    return b_1_dev,b_2_dev,b_1_dev,w_1_dev,w_2_dev,w_3_dev
 
-def update(a):#this is not right but FUCK IT DONT CARE
-   return 1
+def update(a):#UPDATE!!
+   return a
 
 ######-----------------caculating the error and values--------------------######
+
+#straight up cntrl+c / cntrl+v
+
+def get_predictions(A2):
+    return np.argmax(A2, 0)
+
+def get_accuracy(predictions, Y):
+    print(predictions, Y)
+    return np.sum(predictions == Y) / Y.size
+
+def gradient_descent(X, Y, alpha, iterations):
+    for i in range(iterations):
+        Z1,Z2,Z3,A1,A2,A3 = forward_prop(X,weig_1,weig_2,weig_3,bias_1,bias_2,bias_3)
+        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if i % 10 == 0:
+            print("Iteration: ", i)
+            predictions = get_predictions(A2)
+            print(get_accuracy(predictions, Y))
+    return W1, b1, W2, b2
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 500)
